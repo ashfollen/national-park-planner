ebc474670dd1187e49920f151a7674911415740b
Ashley Follen <ashleyfollen@outlook.com>
2021-10-23 15:53:52 -0600

    my feature built

--- src/components/NewFeature.js
+++ src/components/NewFeature.js
@@ -1,9 +1,24 @@
 function NewFeature() {
     return(
-        <>
-           `Everyone's gonna love my new feature`
-            <button>I even have a button</button>
-        </>
+        <div>
+            <p>Start Date:</p>
+            <DatePicker
+                selected={startDate}
+                selectsStart
+                startDate={startDate}
+                endDate={endDate} 
+                onChange={date => setStartDate(date)}
+            />
+            <p>End Date:</p>
+            <DatePicker
+                selected={endDate}
+                selectsEnd
+                startDate={startDate}
+                endDate={endDate}
+                minDate={startDate}
+                onChange={date => setEndDate(date)}
+            />
+   </div>
     )
 }
 
