623dbc5b5309be019b89c4f37e95a382865b1a9e
Ashley Follen <ashleyfollen@outlook.com>
2021-10-23 15:32:29 -0600

    newer feature built

--- src/components/NewFeature.js
+++ src/components/NewFeature.js
@@ -1,10 +1,10 @@
 function NewFeature() {
     return(
         <div>
-            <h1>This is my new feature.</h1>
-            <h2>I'm going to work on this, then push it to GitHub.</h2>
-            <p>If everyone likes it, it will get merged with the develop branch.</p>
-            <p>Maybe later it will be used in the deployed, main branch!</p>
+            <h1>This is MY new feature.</h1>
+            <h2>I think my new feature will be WAY better than developer1's new feature.</h2>
+            <p>I'll show everyone how much better my code is.</p>
+            <p>I bet nobody else used this same component name.</p>
         </div>
     )
 }
