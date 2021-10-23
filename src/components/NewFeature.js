1fe92302b2efa978bf3eaed49a2507ed2729e684
Ashley Follen <ashleyfollen@outlook.com>
2021-10-23 15:46:58 -0600

    feature built

--- src/components/NewFeature.js
+++ src/components/NewFeature.js
@@ -1,10 +1,10 @@
 function NewFeature() {
     return(
-        <div>
-            <h1>This is MY new feature.</h1>
-            <h2>I think my new feature will be WAY better than developer1's new feature.</h2>
-            <p>I'll show everyone how much better my code is.</p>
-            <p>I bet nobody else used this same component name.</p>
-        </div>
+        <>
+           `Everyone's gonna love my new feature`
+            <button>I even have a button</button>
+        </>
     )
 }
+
+export default NewFeature;
